cfbee9ed6a00d66ac107975d69dd88afb27b7764
senlink <senlin_k@163.com>
2016-12-10 21:22:54 +0800

    添加心跳

--- KSLDemo/KSLDemo/Controllers/Animations/ImplicitAnimationViewController.m
+++ KSLDemo/KSLDemo/Controllers/Animations/ImplicitAnimationViewController.m
@@ -9,10 +9,16 @@
 #import "ImplicitAnimationViewController.h"
 #import "UIColor+KSLExtension.h"
 
+/**
+ *  每一个uiview都默认关联着一个CALayer，我们成这个layer为root layer
+ 所有的非root layer都存在默认的隐私动画，隐式动画默认为1/4秒。
+ 如果关闭默认的动画效果：可以通过动画的事务方法实现。
+ */
+
 @interface ImplicitAnimationViewController ()
 
 @property (nonatomic, weak)CALayer *greenLayer;
-@property (nonatomic, weak)UIView *redView;
+@property (nonatomic, weak)UIImageView *redView;
 
 @end
 
@@ -29,24 +35,31 @@
 - (void)setupSubviews
 {
     //UIView中的center就是layer中的position
-    CALayer *layer = [CALayer layer];
-    layer.bounds = CGRectMake(0, 0, 100, 100);
-    layer.position = CGPointMake(100, 64 + 100);
-    layer.anchorPoint = CGPointMake(0, 0);
-    layer.backgroundColor = [UIColor greenColor].CGColor;
-    self.greenLayer = layer;
-    [self.view.layer addSublayer:layer];
+//    CALayer *layer = [CALayer layer];
+//    layer.bounds = CGRectMake(0, 0, 100, 100);
+//    layer.position = CGPointMake(100, 64 + 100);
+//    layer.anchorPoint = CGPointMake(0, 0);
+//    layer.backgroundColor = [UIColor greenColor].CGColor;
+//    self.greenLayer = layer;
+//    [self.view.layer addSublayer:layer];
     
-    UIView *aView = [[UIView alloc] init];
-    [aView setBackgroundColor:[UIColor redColor]];
+    UIImageView *aView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"heart"]];
+    [aView setBackgroundColor:[UIColor clearColor]];
     aView.center = CGPointMake(100 + 10 + 100, 64 + 100);
-    aView.bounds = CGRectMake(0, 0, 100, 100);
-    aView.layer.anchorPoint = CGPointMake(0, 0);
+    //aView.bounds = CGRectMake(0, 0, 100, 100);
+    //动画是绕着anchorPoint做的
+    //aView.layer.anchorPoint = CGPointMake(0, 0);
     [self.view addSubview:aView];
     self.redView = aView;
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    //[self implicitAnimation];
+    [self heartbeat];
+}
+
+- (void)implicitAnimation
 {
     self.redView.center = CGPointMake(100, 64 + 100);
     self.redView.layer.anchorPoint = CGPointMake(0, 0);
@@ -60,6 +73,30 @@
     self.greenLayer.position = CGPointMake([self randomValue], [self randomValue]);
     self.greenLayer.backgroundColor = [UIColor ksl_randomColor].CGColor;
     [CATransaction commit];
+}
+
+- (void)heartbeat
+{
+    //创建动画对象
+    CABasicAnimation *anim = [CABasicAnimation animation];
+    //设置属性
+    anim.keyPath = @"transform.scale";
+    //设置属性值.
+    anim.toValue = @0;
+    
+    //设置动画的执行次数
+    anim.repeatCount = MAXFLOAT;
+    
+    //设置动画的执行时长
+    anim.duration = 0.5;
+    
+    //自动反转
+    anim.autoreverses = YES;
+    
+    //添加动画
+    [self.redView.layer addAnimation:anim forKey:nil];
+    
+
 }
 
 - (CGFloat)randomValue
